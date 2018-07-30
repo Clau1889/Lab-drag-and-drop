3120875d59c095bab975efe8f307efcd0ebbae3a
clau1889 <clau.blanco1889@gmail.com>
2018-07-30 18:11:55 -0500

    En este commit se modifican funciones para poder realizar el drop dentro del contenedor box-top

--- app/app.js
+++ app/app.js
@@ -1,25 +1,13 @@
-function toDrag (){
-    pic1= document.getElementById("pic_1");
-    pic1.addEventListener("dragstart", starDrag, false);
-
-    pic2= document.getElementById("pic_2");
-    pic2.addEventListener("dragstart", starDrag, false);
-
-    pic3= document.getElementById("pic_3");
-    pic3.addEventListener("dragstart", starDrag, false);
-
-    pic4= document.getElementById("pic_4");
-    pic4.addEventListener("dragstart", starDrag, false);
-
-    pic5= document.getElementById("pic_5");
-    pic5.addEventListener("dragstart", starDrag, false);
-
-    pic6= document.getElementById("pic_6");
-    pic6.addEventListener("dragstart", starDrag, false);
-
-    topBox= document.getElementById("bottom-box");
-    topBox.addEventListener("dragenter",function(e){e.preventDefault();},false);
-    topBox.addEventListener("dragover",function(e){e.preventDefault();},false);
-    topBox.addEventListener("drop", dropped ,false);
+function allowDrop(e) {
+    e.preventDefault();
 }
-window.addEventListener("load", toDrag, false);
+
+function drag(e) {
+    e.dataTransfer.setData("text", e.target.id);
+}
+
+function drop(e) {
+    e.preventDefault();
+    var data = e.dataTransfer.getData("text");
+    e.target.appendChild(document.getElementById(data));
+}
